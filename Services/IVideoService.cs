59a3d45649f8f36e82338dba130f61ee09786d9d
David Pine <david.pine@microsoft.com>
2021-06-01 10:39:50 -0500

    Minor updates

--- Services/IVideoService.cs
+++ Services/IVideoService.cs
@@ -6,8 +6,14 @@ namespace IEvangelist.VideoChat.Services
 {
     public interface IVideoService
     {
+        /// <summary>
+        /// Gets the Twilio JSON web token so the client can connect to video services.
+        /// </summary>
         string GetTwilioJwt(string identity);
 
+        /// <summary>
+        /// Gets all of the current active rooms for the app.
+        /// </summary>
         Task<IEnumerable<RoomDetails>> GetAllRoomsAsync();
     }
 }
